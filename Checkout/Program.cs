7dc0134050ec94a68a6181eedebb54a5ada585ff
DanielG-N <dagonzalez@student.neumont.edu>
2022-07-20 14:14:54 -0600

    checkout + email eureka

--- Checkout/Program.cs
+++ Checkout/Program.cs
@@ -1,6 +1,13 @@
+using Steeltoe.Discovery.Client;
+using Steeltoe.Common.Discovery;
+using Steeltoe.Discovery.Eureka;
+using Steeltoe.Discovery;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDiscoveryClient(builder.Configuration);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+//https://localhost:7252;
